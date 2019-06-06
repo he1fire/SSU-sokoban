fe6114523c55c1fe915eb73043a97cb924448ea5
he1fire <cmg3172@naver.com>
2019-06-06 23:56:14 +0900

    리플레이, 나가기, 레벨초기화 구현

--- sokoban.c
+++ sokoban.c
@@ -3,9 +3,11 @@
 #include <windows.h>
 char username[10]; // 유저 이름
 int level=0; // 현재 라운드
+int ex=0; // 게임종료 체크
 int cntmv=0; // 이동 횟수
 
 int getch(); // getch함수 생성
+int CheckClear(); // 맵 클리어 체크 함수
 void Command(); // 명령어 실행 함수
 
 int getch(){ // getch함수 생성
@@ -25,8 +27,12 @@ int getch(){ // getch함수 생성
 int main() { // .==빈칸, @==캐릭터, #==벽, $==박스, O==박스를 채울곳
     for (;level<5;level++){
         Command();
+        if (ex) // 게임 종료 명령
+            break;
         cntmv=0;
     }
+    if (!ex)
+        printf("\n모든맵을 클리어 하였습니다\n");
     return 0;
 }
 
@@ -40,13 +46,17 @@ void Command() { // 명령어 실행 함수
         if (cmd=='h' || cmd=='j' || cmd=='k' || cmd=='l') // 이동 명령
             ;
         if (cmd=='n'){ // 처음부터 다시시작 명령
-            ;
+            level=-1;
+            printf("처음부터 다시시작합니다.\n");
+            break;
         }
         if (cmd=='r'){ // 이번 맵 다시시작 명령
-            ;
+            printf("이번 맵을 다시시작합니다.\n");
         }
         if (cmd=='e'){ // 게임 종료 명령
-            ;
+            ex=1;
+            printf("게임을 종료합니다.\n");
+            break;
         }
         if (cmd=='x') {// 임시 클리어 명령어 (나중에 지울예정)
             break;
@@ -62,6 +72,9 @@ void Command() { // 명령어 실행 함수
         }
         if (cmd=='u') // 맵 되돌리기 명령
             ;
+        /*if (){ 맵 클리어 체크
+            printf("Clear Map! %s!\n총 이동횟수는 %d번 입니다\n", username, cntmv);
+            break;
+        }*/
     }
 }
-
